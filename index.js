7e64e920ac1f1e91250dab1ed56117158ba8a803
nemitha2005 <nemithan05@gmail.com>
2025-03-27 18:49:00 +0530

    feat: added database connection and functionality

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static("public"));
+
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("Connected to DB");
+        app.listen(3000, () => console.log("Server Up and Running"));
+    })
+    .catch((err) => console.log(err));
+
+
 app.set("view engine", "ejs");
+
 
 app.get('/', (req, res) => {
     res.render('todo.ejs')
 })
 
-app.listen(3000,()=>console.log("Server Up and Running"))
+app.post('/', (req, res) => {
+    console.log(req.body);
+})
+
+// app.listen(3000,()=>console.log("Server Up and Running"))
