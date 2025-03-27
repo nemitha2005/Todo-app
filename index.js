b5376027d934c6f7df12c2037ed04a682756c16d
nemitha2005 <nemithan05@gmail.com>
2025-03-27 19:06:25 +0530

    feat: add todo task functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
 
 dotenv.config();
 
@@ -23,8 +24,17 @@ app.get('/', (req, res) => {
     res.render('todo.ejs')
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-})
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    })
+    try{
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send(err);
+        res.redirect("/");
+    }
+});
 
 // app.listen(3000,()=>console.log("Server Up and Running"))
